67cd3c63778417d07b2bb86774326ae070bb402b
mohammedAlefrangy <mohamed.alefrangy@gmail.com>
2019-03-05 14:53:44 +0200

    1- update my rent activity 2- add MyBounceInterpolator

--- app/src/main/java/com/example/hmod_/bike/Activity/ConnectBike.java
+++ app/src/main/java/com/example/hmod_/bike/Activity/ConnectBike.java
@@ -1,6 +1,7 @@
 package com.example.hmod_.bike.Activity;
 
 
+import android.content.res.TypedArray;
 import android.support.v4.app.Fragment;
 import android.content.Intent;
 import android.os.Bundle;
@@ -10,13 +11,19 @@ import android.support.v7.app.AppCompatActivity;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.view.animation.Animation;
+import android.view.animation.AnimationUtils;
 import android.widget.ArrayAdapter;
+import android.widget.Button;
+import android.widget.ImageButton;
 import android.widget.ImageView;
 import android.widget.Spinner;
 
 import com.example.hmod_.bike.R;
 
 import java.util.Objects;
+import java.util.Timer;
+import java.util.TimerTask;
 
 import butterknife.BindView;
 import butterknife.ButterKnife;
@@ -27,7 +34,7 @@ public class ConnectBike extends Fragment {
     Intent intentThatStartedThisActivity ;
 
     @BindView(R.id.rent_now)
-    ImageView rent_now;
+    Button rent_now;
     @BindView(R.id.spinner)
     Spinner spinner;
 
@@ -47,6 +54,52 @@ public class ConnectBike extends Fragment {
         // Apply the adapter to the spinner
         spinner.setAdapter(adapter);
 
+
+
+
+        final Animation myAnim = AnimationUtils.loadAnimation(getContext(), R.anim.bounce);
+
+        // Use bounce interpolator with amplitude 0.2 and frequency 20
+        MyBounceInterpolator interpolator = new MyBounceInterpolator(0.2, 10);
+        myAnim.setInterpolator(interpolator);
+
+        Timer timer = new Timer();
+        //Set the schedule function
+        timer.scheduleAtFixedRate(new TimerTask() {
+
+                                      @Override
+                                      public void run() {
+                                          // Magic here
+                                          rent_now.startAnimation(myAnim);
+                                      }
+                                  },
+                0, 1000); // 1000 Millisecond  = 1 second
+
+
+        rent_now.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+
+//                final Animation myAnim = AnimationUtils.loadAnimation(getContext(), R.anim.bounce);
+//
+//                // Use bounce interpolator with amplitude 0.2 and frequency 20
+//                MyBounceInterpolator interpolator = new MyBounceInterpolator(0.2, 10);
+//                myAnim.setInterpolator(interpolator);
+//
+//                rent_now.startAnimation(myAnim);
+
+//                final Animation myAnim = AnimationUtils.loadAnimation(getContext(), R.anim.bounce);
+//                rent_now.startAnimation(myAnim);
+//                int[] attrs = new int[]{R.attr.selectableItemBackground};
+//                TypedArray typedArray = getActivity().obtainStyledAttributes(attrs);
+//                int backgroundResource = typedArray.getResourceId(0, 1);
+//                rent_now.setBackgroundResource(backgroundResource);
+
+            }
+        });
+
+
+
         return rootView ;
     }
 
@@ -55,6 +108,5 @@ public class ConnectBike extends Fragment {
 //        super.onBackPressed();
 //
 //    }
-
 
 }
