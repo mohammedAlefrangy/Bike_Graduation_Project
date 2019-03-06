2c83995d4eebd8ae5c7e6d91f8cf32529075d899
mohammedAlefrangy <mohamed.alefrangy@gmail.com>
2019-03-07 00:00:53 +0200

    1- remove some imports

--- app/src/main/java/com/example/hmod_/bike/Activity/ConnectBike.java
+++ app/src/main/java/com/example/hmod_/bike/Activity/ConnectBike.java
@@ -7,7 +7,6 @@ import android.content.BroadcastReceiver;
 import android.content.Context;
 import android.content.Intent;
 import android.content.IntentFilter;
-import android.graphics.drawable.AnimatedVectorDrawable;
 import android.os.Build;
 import android.os.Bundle;
 import android.support.annotation.NonNull;
